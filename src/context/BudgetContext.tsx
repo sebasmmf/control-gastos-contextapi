8bde3e3c3d1bdde4deea5ff210b3faf2b7997bf3
sebasmmf <sebastian.mart.f@gmail.com>
2024-07-25 20:47:41 -0500

    Proyecto planificador de gastos

---
+++ src/context/BudgetContext.tsx
@@ -0,0 +1,35 @@
+import { useMemo ,useReducer, createContext, Dispatch, ReactNode } from "react"
+import { budgetReducer, BudgetState, initialState, budgetActions } from "../reducers/budget-reducer"
+
+type BudgetContextProps = {
+    state: BudgetState,
+    dispatch: Dispatch<budgetActions>,
+    totalExpenses: number,
+    remainingBudget: number
+}
+
+type BudgetProviderProps = {
+    children: ReactNode
+}
+
+export const BudgetContext = createContext<BudgetContextProps>(null!)
+
+export const BudgetProvider = ({children} : BudgetProviderProps) => {
+
+    const [state, dispatch] = useReducer(budgetReducer, initialState)
+    const totalExpenses = useMemo(() => state.expenses.reduce((total, expense) => expense.amount + total, 0), [state.expenses])
+    const remainingBudget = state.budget - totalExpenses
+
+    return (
+        <BudgetContext.Provider
+            value={{
+                state,
+                dispatch,
+                totalExpenses,
+                remainingBudget
+            }}
+        >
+            {children}
+        </BudgetContext.Provider>
+    )
+}
